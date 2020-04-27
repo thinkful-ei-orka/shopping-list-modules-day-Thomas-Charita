6ea101ab38f38330a43656e046d873f793b6f3ec
Charita Nichols <nichols.charita@gmail.com>
2020-04-27 18:06:46 -0400

    asignment

--- scripts/store.js
+++ scripts/store.js
@@ -3,15 +3,55 @@ let items = [];
 let hideCheckedItems = false;
 
 const findById = function (id) {
-    items.find(element => function {
-        
-    })
+    return items.find(item => item.id === id);
 }
+
+const addItem = function (name) {
+    try {
+        item.validateName(name);
+        let newitem = item.create(name);
+        items.push(newitem);
+      } 
+      catch (error) {
+        console.log(`Cannot add item:${error.message}`);
+      }
+}
+
+const findAndToggleChecked = function(id) {
+    let item = this.findById(id);
+    item.checked = !item.checked;
+}
+
+const findAndUpdateName = function(id, newName) {
+    try{
+        item.validateName(newName);
+        let itemEdit = this.findById(id);
+        itemEdit.name = newName;
+    }
+    catch (error) {
+        console.log(`Cannot add item:${error.message}`);
+    }
+}
+
+const findAndDelete = function(id) {
+    this.items = items.filter(item => item.id !== id);
+}
+
+const toggleCheckedFilter = function() {
+    this.hideCheckedItems = !hideCheckedItems; 
+}
+
 
 
 
 export default {
 items,
-hideCheckedItems
+hideCheckedItems,
+addItem,
+findById,
+findAndToggleChecked,
+findAndUpdateName,
+findAndDelete,
+toggleCheckedFilter
 };
 
